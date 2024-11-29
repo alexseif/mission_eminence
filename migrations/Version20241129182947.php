3fbb6f47dd92b31935d0596674fe387b3d86ae4e
Alex Seif <alex.seif@gmail.com>
2024-11-29 20:30:31 +0200

    chore(course): doctrine migration

--- migrations/Version20241129182947.php
+++ migrations/Version20241129182947.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20241129182214 extends AbstractMigration
+final class Version20241129182947 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20241129182214 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE course CHANGE name title VARCHAR(255) NOT NULL');
+        $this->addSql('CREATE TABLE course (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(255) NOT NULL, vidoe_embed VARCHAR(255) NOT NULL, description LONGTEXT DEFAULT NULL, day INT DEFAULT NULL, image VARCHAR(255) DEFAULT NULL, locked TINYINT(1) NOT NULL, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE course CHANGE title name VARCHAR(255) NOT NULL');
+        $this->addSql('DROP TABLE course');
     }
 }
