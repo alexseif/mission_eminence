b4bfd02b924119ef482c78bb0bba277181eea9dd
Alex Seif <alex.seif@gmail.com>
2025-01-29 15:51:57 +0200

    chore: admin stats

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -2,6 +2,12 @@
 
 namespace App\Controller\Admin;
 
+use App\Repository\CourseRepository;
+use App\Repository\UserRepository;
+use App\Repository\TestimonialRepository;
+use App\Repository\PartnershipRepository;
+use App\Repository\MMMRepository;
+use App\Repository\EventRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -12,8 +18,25 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 class AdminController extends AbstractController
 {
     #[Route('/', name: 'admin_home')]
-    public function index(): Response
-    {
-        return $this->render('admin/index.html.twig');
+    public function index(
+        UserRepository $userRepository,
+        CourseRepository $courseRepository,
+        TestimonialRepository $testimonialRepository,
+        PartnershipRepository $partnershipRepository,
+        MMMRepository $mmmRepository,
+        EventRepository $eventRepository
+    ): Response {
+        $stats = [
+            'students' => $userRepository->count(['roles' => ['ROLE_STUDENT']]),
+            'courses' => $courseRepository->count([]),
+            'testimonials' => $testimonialRepository->count([]),
+            'partnerships' => $partnershipRepository->count([]),
+            'mmm' => $mmmRepository->count([]),
+            'events' => $eventRepository->count([])
+        ];
+
+        return $this->render('admin/index.html.twig', [
+            'stats' => $stats
+        ]);
     }
 }
