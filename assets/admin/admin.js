019823bf388fa60e24256fc11c7b1ae906c50569
Alex Seif <alex.seif@gmail.com>
2025-01-29 18:27:40 +0200

    chore: enroll modal and form validation

--- assets/admin/admin.js
+++ assets/admin/admin.js
@@ -4,3 +4,17 @@ import './styles/admin.scss';
 import 'bootstrap';
 
 import 'bootstrap-icons/font/bootstrap-icons.css';
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.querySelector('.needs-validation');
+    if (form) {
+        form.addEventListener('submit', function (event) {
+            if (!form.checkValidity()) {
+                event.preventDefault();
+                event.stopPropagation();
+            }
+            form.classList.add('was-validated');
+        }, false);
+    }
+});
