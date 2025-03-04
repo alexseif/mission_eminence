e8acadb3215843a78757763697d837ed5d72d9ef
Alex Seif <alex.seif@gmail.com>
2025-03-04 23:32:22 +0200

    build: namecheap deployer

--- deployer.sh
+++ deployer.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 git reset HEAD --hard
 git pull origin master
-php8.2 /usr/local/bin/composer dump-env prod
-php8.2 /usr/local/bin/composer install --no-dev --optimize-autoloader
-APP_ENV=prod APP_DEBUG=0 php8.2 bin/console cache:clear --env=prod
-php8.2 bin/console doctrine:migrations:migrate --no-interaction --env=prod
+php /opt/cpanel/composer/bin/composer dump-env prod
+php /opt/cpanel/composer/bin/composer install --no-dev --optimize-autoloader
+APP_ENV=prod APP_DEBUG=0 php bin/console cache:clear --env=prod
+php bin/console doctrine:migrations:migrate --no-interaction --env=prod
 npm install
 npm run build
